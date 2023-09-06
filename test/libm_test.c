bbd2e1a198f9846b0b249eccb9b9f341eeb9485d
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2023-09-06 17:02:59 -0500

    update

---
+++ test/libm_test.c
@@ -0,0 +1,124 @@
+#include <libgen.h>
+#include <pwd.h>
+#include <stdlib.h>             /* putenv */
+#include <unistd.h>             /* getcwd */
+#include <sys/errno.h>
+#include <sys/types.h>
+
+#include "gopt.h"
+#include "log.h"
+#include "unity.h"
+#include "utarray.h"
+#include "utstring.h"
+
+#include "s7plugin_test_config.h"
+/* #include "libs7.h" */
+
+s7_scheme *s7;
+
+extern struct option options[];
+
+char *sexp_input;
+char *sexp_expected;
+
+UT_string *setter;
+UT_string *sexp;
+s7_pointer actual;
+s7_pointer expected;
+
+bool verbose;
+bool debug;
+
+/* WARNING: setUp and tearDown are run once per test. */
+void setUp(void) {
+    /* log_info("setup"); */
+}
+
+void tearDown(void) {
+    /* log_info("teardown"); */
+}
+
+/* s7_flush_output_port(s7, s7_current_output_port(s7)); */
+/* char *s = s7_object_to_c_string(s7, actual); */
+/* log_debug("result: %s", s); */
+/* free(s); */
+
+void test_math(void) {
+    sexp_input = "(+ 2 3)";
+    sexp_expected = "5";
+    utstring_renew(sexp);
+    utstring_printf(sexp, "%s", sexp_input);
+    actual = s7_eval_c_string(s7, utstring_body(sexp));
+    expected = s7_eval_c_string(s7, sexp_expected);
+    TEST_ASSERT_TRUE(s7_is_equal(s7, actual, expected));
+}
+
+void test_libm(void) {
+    sexp_input = "(libm:pow 2 3)";
+    sexp_expected = "8.0";
+    utstring_renew(sexp);
+    utstring_printf(sexp, "%s", sexp_input);
+    actual = s7_eval_c_string(s7, utstring_body(sexp));
+    expected = s7_eval_c_string(s7, sexp_expected);
+    TEST_ASSERT_TRUE(s7_is_equal(s7, actual, expected));
+
+    sexp_input = "(libm:ceil 2.3)";
+    sexp_expected = "3.0";
+    utstring_renew(sexp);
+    utstring_printf(sexp, "%s", sexp_input);
+    actual = s7_eval_c_string(s7, utstring_body(sexp));
+    expected = s7_eval_c_string(s7, sexp_expected);
+    TEST_ASSERT_TRUE(s7_is_equal(s7, actual, expected));
+
+    sexp_input = "(libm:floor 2.3)";
+    sexp_expected = "2.0";
+    utstring_renew(sexp);
+    utstring_printf(sexp, "%s", sexp_input);
+    actual = s7_eval_c_string(s7, utstring_body(sexp));
+    expected = s7_eval_c_string(s7, sexp_expected);
+    TEST_ASSERT_TRUE(s7_is_equal(s7, actual, expected));
+
+    sexp_input = "(libm:fmod 7 2)";
+    sexp_expected = "1.0";
+    utstring_renew(sexp);
+    utstring_printf(sexp, "%s", sexp_input);
+    actual = s7_eval_c_string(s7, utstring_body(sexp));
+    expected = s7_eval_c_string(s7, sexp_expected);
+    TEST_ASSERT_TRUE(s7_is_equal(s7, actual, expected));
+
+    sexp_input = "(libm:fmod 7 3)";
+    sexp_expected = "1.0";
+    utstring_renew(sexp);
+    utstring_printf(sexp, "%s", sexp_input);
+    actual = s7_eval_c_string(s7, utstring_body(sexp));
+    expected = s7_eval_c_string(s7, sexp_expected);
+    TEST_ASSERT_TRUE(s7_is_equal(s7, actual, expected));
+}
+
+int main(int argc, char **argv)
+{
+    s7 = s7_plugin_initialize("libm", argc, argv);
+
+    libs7_load_clib(s7, "m");
+
+    char *script_dir = "./test";
+    s7_pointer newpath;
+    newpath =  s7_add_to_load_path(s7, script_dir);
+    (void)newpath;
+
+    /* debugging: */
+    /* s7_pointer loadpath = s7_load_path(s7); */
+    /* char *s = s7_object_to_c_string(s7, loadpath); */
+    /* log_debug("load path: %s", s); */
+    /* free(s); */
+
+    utstring_new(sexp);
+
+    UNITY_BEGIN();
+
+    RUN_TEST(test_math);
+    RUN_TEST(test_libm);
+
+    /* utstring_free(sexp); */
+    return UNITY_END();
+}
